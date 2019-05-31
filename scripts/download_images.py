d0fdf6f2514489188e343ecb4cf54e35189b8d1e
R. Biswas <rbiswas4@gmail.com>
2019-05-31 10:50:06 +0200

    Some downloads sarted .. 	modified:   download_images.py

--- scripts/download_images.py
+++ scripts/download_images.py
@@ -6,27 +6,36 @@ import ztfquery
 from ztfquery import query
 
 def query_string(rcid):
-    return "expid in {0} and rcid == {1}".format(tuple(x for x in grouped.get_group(rcid).expid.values), rcid)
+    return "expid in {0} and rcid = {1}".format(tuple(x for x in grouped.get_group(rcid).expid.values), rcid)
 
-#def download_metadata(corner_df):
-#    """
-#    """
-#    dfs = []
-#    for (ra, dec) in df[['ra', 'dec']]:
-#        zquery.load_metadata(radec=[ra, dec], size=0.01)
-#        dfs.append(zquery.metatable)
-#    meta = pd.concat(dfs)
-#    return meta.drop_duplicates(subset=['expid', 'rcid'])
+def download_metadata(corner_df):
+    """
+    """
+    zquery = query.ZTFQuery()
+    dfs = []
+    for (ra, dec) in df[['ra', 'dec']].values:
+        zquery.load_metadata(radec=[ra, dec], size=0.01)
+        dfs.append(zquery.metatable)
+    meta = pd.concat(dfs)
+    return meta.drop_duplicates(subset=['expid', 'rcid'])
 
+zquery = query.ZTFQuery()
 df = pd.read_csv('corners.csv')
+print("got data from corners")
 meta = download_metadata(df)
 utable = meta.drop_duplicates(subset=('expid', 'rcid'))
+print('Obtained metadata from ZTF')
+utable.to_csv('unique_table.csv', index=False)
 rcids = utable.rcid.unique()
+print (rcids)
 grouped = utable.groupby('rcid')
-zquery = query.ZTFQuery()
 
 
 for rcid in rcids:
     q = query_string(rcid)
+    print(q)
     zquery.load_metadata(sql_query=q)
-    zquery.download_data("sciimg.fits", show_progress=True)
+    try:
+        zquery.download_data("sciimg.fits", show_progress=True, nprocess=4)
+    except:
+        print('image download with {} rcid failed'.format(rcid))
